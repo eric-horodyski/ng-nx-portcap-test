60b94d1b0950eabb48f0a12e41aca3beabfd93a2
Eric Horodyski <horodyski@ionic.io>
2022-09-27 10:47:21 -0400

    step(1): add shell app

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-nx-portcap-test-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
